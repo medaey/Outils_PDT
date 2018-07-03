01804a90dc58996b3db1023d0379a20d506bf857
Leghort <leghort@outlook.fr>
2018-07-03 20:51:16 +0200

    Add files via upload

---
+++ outils_PDT[ALL].bat
@@ -0,0 +1,140 @@
+rem Auteur : mederic@cossu.tech
+setlocal
+@echo off
+chcp437 > nul
+@for %%n in (%0) do set fold=%%~dpn
+title Boite A Outils Technicien Poste De Travail                                                  By M.Cossu
+mode con cols=100 lines=40
+
+:check_admin
+	rem v‚rifier ci le script est bien ‚xecuter en Administrateur
+		attrib %windir%\system32 -h | findstr /I "system32" >nul
+		IF %errorlevel% neq 1 (
+		color 0C
+		echo.
+		echo. /!\ Ce script doit etre Ex‚cuter en tant qu'Administrateur /!\
+		echo.
+		pause
+		exit
+       )
+
+:menu
+	color 0b
+	IF exist "%fold%ressource" (echo Dossier "ressource" trouver.) else goto :erreur
+	cls
+	echo                                =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	echo                                 ÛÛÛÛÛ» ÛÛÛÛÛÛ» ÛÛÛ»   ÛÛÛ»ÛÛ»ÛÛÛ»   ÛÛ»
+	echo                                ÛÛÉÍÍÛÛ»ÛÛÉÍÍÛÛ»ÛÛÛÛ» ÛÛÛÛºÛÛºÛÛÛÛ»  ÛÛº
+	echo                                ÛÛÛÛÛÛÛºÛÛº  ÛÛºÛÛÉÛÛÛÛÉÛÛºÛÛºÛÛÉÛÛ» ÛÛº
+	echo                                ÛÛÉÍÍÛÛºÛÛº  ÛÛºÛÛºÈÛÛÉ¼ÛÛºÛÛºÛÛºÈÛÛ»ÛÛº
+	echo                                ÛÛº  ÛÛºÛÛÛÛÛÛÉ¼ÛÛº ÈÍ¼ ÛÛºÛÛºÛÛº ÈÛÛÛÛº
+	echo                                ÈÍ¼  ÈÍ¼ÈÍÍÍÍÍ¼ ÈÍ¼     ÈÍ¼ÈÍ¼ÈÍ¼  ÈÍÍÍ¼
+	echo                                                Page (1/2)
+	echo                                =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	echo.
+	echo.
+	echo Executer depuis : %fold% 
+	echo ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+	IF exist "%fold%\ressource\autologon.bat" (echo  [OK]  1: G‚rer l'Autologon
+		) else echo [FAIL] 1: G‚rer l'Autologon
+	echo ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+	IF exist "%fold%\ressource\restart_network.bat" (echo  [OK]  2: R‚parer le services r‚seau HS
+		) else echo [FAIL] 2: R‚parer le services r‚seau HS
+	echo ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+	echo Q: Quitter
+	echo ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+	echo.
+	echo.
+	echo.
+	set /p reponse="Quel programme voulez-vous ex‚cuter ?"
+
+	IF /i "%reponse%"=="1" goto :batch1
+	IF /i "%reponse%"=="2" goto :batch2
+	IF /i "%reponse%"=="3" goto :batch3
+	IF /i "%reponse%"=="4" goto :batch4
+	IF /i "%reponse%"=="5" goto :batch5
+	IF /i "%reponse%"=="6" goto :batch6
+	IF /i "%reponse%"=="q" goto :fin
+	IF /i "%reponse%"=="Q" goto :fin
+	IF /i "%reponse%"=="" goto :menu
+	goto :menu
+
+:batch1
+	cls
+	IF exist "%fold%\ressource\autologon.bat" (call %fold%\ressource\autologon.bat
+		) else goto :erreur_batch1
+	cls
+	goto :menu
+
+:batch2
+	cls
+	IF exist "%fold%\ressource\restart_network.bat" (call %fold%\ressource\restart_network.bat
+		) else goto :erreur_batch2
+	cls
+	goto :menu
+
+:batch3
+	cls
+	call %fold%\ressource\Mon_ping.bat
+	cls
+	goto :menu
+
+:batch4
+	cls
+	call %fold%\ressource\Mon_ipconfig.bat 
+	cls
+	goto :menu
+
+:batch5
+	cls
+	call %fold%\ressource\windows_update.bat
+	cls
+	goto :menu
+
+:batch6
+	cls
+	call %fold%\ressource\nettoyage.bat
+	cls
+	goto :menu
+
+:erreur
+	cls
+	echo Dossier "%fold%ressource" non trouver.
+	pause
+	cls
+	goto :fin
+
+:erreur_batch1
+	color 0C
+	cls
+	echo.
+	echo.
+	echo ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+	echo  Le fichier "%fold%ressource\autologon.bat" n'a pu ˆtre trouv‚e!
+	echo  Merci de v‚rifier que le fichier "autologon.bat"
+	echo  est bien pr‚sent dans "%fold%ressource"
+	echo  (cette fontionnalit‚e ne sera pas disponible tant que le problŠme ne sera pas r‚solut)
+	echo ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+	echo.
+	echo.
+	pause
+	goto :menu
+
+:erreur_batch2
+	color 0C
+	cls
+	echo.
+	echo.
+	echo ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+	echo  Le fichier "%fold%ressource\restart_network.bat" n'a pu ˆtre trouv‚e!
+	echo  Merci de v‚rifier que le fichier "restart_network.bat"
+	echo  est bien pr‚sent dans "%fold%ressource"
+	echo  (cette fontionnalit‚e ne sera pas disponible tant que le problŠme ne sera pas r‚solut)
+	echo ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+	echo.
+	echo.
+	pause
+	goto :menu
+:fin
+	endlocal
+	exit
---
+++ ressource/restart_network.bat
@@ -0,0 +1,39 @@
+rem Auteur : mederic@cossu.tech
+rem Version : 1.0.3
+setlocal
+@echo off
+chcp437 > nul
+@for %%n in (%0) do set fold=%%~dpn
+title Boite A Outils Technicien Poste De Travail                                                  By M.Cossu
+mode con cols=100 lines=40
+
+:check_admin
+	rem v‚rifier ci le script est bien ‚xecuter en Administrateur
+		attrib %windir%\system32 -h | findstr /I "system32" >nul
+		IF %errorlevel% neq 1 (
+		color 0C
+		echo.
+		echo. /!\ Ce script doit etre Ex‚cuter en tant qu'Administrateur /!\
+		echo.
+		pause
+		exit
+       )
+
+	color 0b
+	rem Reparation d'une carte reseaux non detect‚e par windows 7
+	cls
+	echo.
+	echo /!\ Attention c'ette action va red‚marrer l'ordinateur /!\
+	echo.
+	set /p question=Souhaitez vous continuez [O/N]?
+	if /i not "%question%"=="O" goto :retour
+	net localgroup administrateurs utilisateurs /add networkservice localservice
+	shutdown -r -t 5
+	goto :fin
+
+:retour
+	exit /b 0
+
+:fin
+	endlocal
+	exit
